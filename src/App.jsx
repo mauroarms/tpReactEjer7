2f80a106353982471dd92f20b6e782d37eea351a
mauroarms <mauro.armas14@gmail.com>
2024-01-30 10:18:05 -0300

    Se limpió la página y se instalaron dependencias

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "bootstrap/dist/css/bootstrap.min.css"
+import Container from "react-bootstrap/Container";
+function App() {
+
+  return (
+    <>
+      <Container>
+        <h1>Hola Mundo</h1>
+      </Container>
+    </>
+  )
+}
+
+export default App
